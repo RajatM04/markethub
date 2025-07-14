2239d522d756edf4737e2cc94a1f5092d0604cf8
RajatM04 <rajatmahadule2004@gmail.com>
2025-07-14 15:17:23 +0530

    "# This is a commit message for the recent changes made to the project."

--- src/components/BuyerLoginPage.jsx
+++ src/components/BuyerLoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import personImg from '../assets/personImg.png';
-import '../../src/Styles/BuyerLoginPage.css'; // Adjust the path as necessary
+import '../../src/Styles/BuyerLoginPage.css';
 const BuyerLoginPage = () => {
     const navigate = useNavigate();
 
